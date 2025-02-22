181462e8f33b0313edad88a0757b50a4f5ae873c
HarshF40 <harshgaonkar57@gmail.com>
2025-02-22 10:46:15 +0530

    math

---
+++ Basic/maths.java
@@ -0,0 +1,14 @@
+public class maths {
+  public static void main(String[] args) {
+    System.out.println(Math.PI);
+    System.out.println(Math.E);
+    System.out.println(Math.pow(2,3));
+    System.out.println(Math.abs(-5));
+    System.out.println(Math.sqrt(4));
+    System.out.println(Math.round(3.45));
+    System.out.println(Math.ceil(3.45));
+    System.out.println(Math.floor(3.45));
+    System.out.println(Math.max(1,2));
+    System.out.println(Math.min(1,2));
+  }
+}
